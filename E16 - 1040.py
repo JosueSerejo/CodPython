3d521630f8aad93b9b283f9d07f33af3da17b596
Your Name <you@example.com>
2024-10-30 23:17:20 -0300

    Subindo E16 e E17

--- E16 - 1040.py
+++ E16 - 1040.py
@@ -1,16 +1,38 @@
 
-N1 = float(input())
-N2 = float(input())
-N3 = float(input())
-N4 = float(input())
+num = input().split()
+
+n1 = float(num[0])
+n2 = float(num[1])
+n3 = float(num[2])
+n4 = float(num[3])
 
 
-Media = (N1 * 2 + N2 * 3 + N3 * 4 + N4 * 1) / 10
+Media = (n1 * 2 + n2 * 3 + n3 * 4 + n4 * 1) / 10
 
-print(f'{Media:.1f}')
+print(f'Media: {Media:.1f}')
 
-if Media > 5.0:
-    print('Aluno aprovado')
+if Media >= 7.0:
+    print('Aluno aprovado.')
+    exit()
+
+elif Media < 5.0:
+    print('Aluno reprovado.')
+    exit()
 
 else:
-    print('Aluno reprovado')
+    print('Aluno em exame.')
+
+    Exame = float(input(''))
+
+    Final = (Exame + Media) / 2
+
+    print(f'Nota do exame: {Exame:.1f}')
+
+if Final >= 5.0:
+
+    print('Aluno aprovado.')
+
+else:
+    print('Aluno reprovado.')
+
+print(f'Media final: {Final:.1f}')
---
+++ E17 - 1114.py
@@ -0,0 +1,13 @@
+Password = 2002
+
+
+while True:
+    Pass = int(input())
+
+    if Pass != Password:
+        print('Senha Invalida')
+        
+
+    else:
+        print('Acesso Permitido')
+        break
